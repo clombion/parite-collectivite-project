5141d57b9cdd4f89445fd54cb13988e913ae7c8f
clombion <clombion@users.noreply.github.com>
2019-04-11 17:11:32 +0100

    cleaned up script

--- script_categorisation.sh
+++ script_categorisation.sh
@@ -7,29 +7,60 @@
 # this produces a list of files equal to the number of lines of attributions.txt
 # These files are processed to add a space and an /n, making it possible to concatenate them with cat
 
+
+define_variables() {
+    readonly competences="competences.txt"
+    readonly categories="categories.txt"
+    readonly attributions="attributions.txt"
+}
+
 # creating the files with the validated categories
-let i=0
-while read -r var0
-    do
-    while read -r var1 <&3 && read var2 <&4
+# each created file is processed in order to remove line breaks and add space between words (if any)
+
+categorisation() {
+    local var_att
+    local var_cat
+    local var_comp
+    let i=0
+    while read -r var0
         do
-        if [[ ${var0} == *"${var2}"* ]]
-        then echo "${var1} " | tr -d '\n'
-        fi
-    done 3<categories.txt 4<competences.txt  > file_$i.txt
-let i+=1
-done < attributions.txt
+        while read -r var1 <&3 && read var2 <&4
+            do
+            if [[ ${var_att} == *"${var_comp}"* ]]
+            then echo "${var_cat} " | tr -d '\n'
+            fi
+        done 3<${categories} 4<${competences}  > file_$i.txt
+    let i+=1
+    done < ${attributions}
+}
 
 #prepare the files to make sure cat can process them
-let j=0
-for j in file_*
-    do
-    echo ' ' >> $j
-    sed -i '' -e '$a\' $j
-done
+
+file_prep() {
+    local filename
+    for filename in file_*
+        do
+        echo ' ' >> ${filename}
+     sed -i '' -e '$a\' ${filename}
+    done
+}
 
 #compile everything into the final text
-cat file_* > final.txt
+
+compilation() {
+    cat file_* > final.txt
+}
 
 #clean up folder
+
+cleanup() {
 rm file_*
+}
+
+main() {
+define_variables
+categorisation
+file_prep
+compilation
+cleanup
+}
